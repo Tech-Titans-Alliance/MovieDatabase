6c614e56ba0fe097954d37bc0a70b70813f18f1a
unknown <Capaciti@COMP5.bwb.org.za>
2024-11-25 15:59:08 +0200

    core functionality without styling

--- Reel-Radar/src/App.jsx
+++ Reel-Radar/src/App.jsx
@@ -1,33 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import MovieList from './MovieList';
+import Header from './Header';
 
-function App() {
-  const [count, setCount] = useState(0)
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import SearchBox from './SearchBox';
+
+
+const App=()=> {
+  const [movies, setMovies] = useState([]);
+  const [searchValue,setSearchValue]=useState('');
+
+  const GetMovieRequest=async(searchValue)=>{
+    const url=`http://www.omdbapi.com/?s=${searchValue}&apikey=c7301b93`
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    console.log(responseJson);
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+    
+  };
+  //for the api to fetch movies
+  useEffect(()=>{
+    GetMovieRequest(searchValue);
+  },[searchValue]);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     
+    <div className='container-fluid'>
+       
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <Header/>
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+     <div className='row'>
+      <MovieList movies={movies}/>
+    </div> 
+        
+    </div>
+
+      
     </>
   )
 }
---
+++ Reel-Radar/src/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const Header=()=>{
+    return(
+        <header>
+            <h1>Reel Radar</h1>
+            <nav>
+                <ul>
+                    <li><a href='#'>Home</a></li>
+                    
+                    <li><a href='#'>Favourites</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+export default Header
