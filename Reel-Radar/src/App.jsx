1cde0a427e6a68e9608c7990a3a40875aa009948
Palesa Claudia Mashabela <palesaclaudia2@gmail.com>
2024-11-26 15:08:00 +0200

    Add header with home, favourites, and genre dropdown

--- Reel-Radar/src/App.jsx
+++ Reel-Radar/src/App.jsx
@@ -1,56 +1,49 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import MovieList from './MovieList';
 import Header from './Header';
-
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from './components/Footer';
+import './App.css';
 import SearchBox from './SearchBox';
 
-
-const App=()=> {
+const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchValue,setSearchValue]=useState('');
+  const [searchValue, setSearchValue] = useState('');
 
-  
-  const GetMovieRequest=async(searchValue)=>{
-    const url=`http://www.omdbapi.com/?s=${searchValue}&apikey=c7301b93`
+  const GetMovieRequest = async (searchValue) => {
+    // Replace with your desired API URL for fetching movies (this link is just an example)
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=c7301b93`; // Make sure this is replaced with the correct one
     const response = await fetch(url);
     const responseJson = await response.json();
     console.log(responseJson);
-    if(responseJson.Search){
+    if (responseJson.Search) {
       setMovies(responseJson.Search);
     }
-    
   };
-  //for the api to fetch movies
-  useEffect(()=>{
-    GetMovieRequest(searchValue);
-  },[searchValue]);
+
+  // Fetch movies when the search value changes
+  useEffect(() => {
+    if (searchValue) {
+      GetMovieRequest(searchValue);
+    }
+  }, [searchValue]);
 
   return (
     <>
-     
-    <div className='container-fluid'>
-       
-      {/* <div className='row d-flex align-items-center mt-4 mb-4'>
-        <Header/>
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
-        <Header/>
-      </div> */}
+      <div className='container-fluid'>
+        {/* Place Header on top with search functionality */}
+        <Header searchValue={searchValue} setSearchValue={setSearchValue} />
 
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      
-     <div className='row'>
-      <MovieList movies={movies}/>
-    </div> 
-        
-    </div>
+        <div className="row">
+          {/* Render Movie List */}
+          <MovieList movies={movies} />
+        </div>
 
-      
+        {/* Render Footer */}
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Reel-Radar/src/Header.jsx
+++ Reel-Radar/src/Header.jsx
@@ -1,24 +1,81 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Import Bootstrap JS
+import "./header.css"; // Import the custom header CSS
 
 const Header = ({ searchValue, setSearchValue }) => {
-    return (
-        <div className="d-flex justify-content-between align-items-center w-100 bg-primary text-white py-3 px-4">
-            {/* Left-Aligned Title */}
-            <h1 className="m-0">Movie Finder</h1>
+  return (
+    <header className="header">
+      {/* Search Box */}
+      <div className="container-fluid search-container py-3">
+        <input
+          className="form-control search-input"
+          type="text"
+          value={searchValue}
+          onChange={(event) => setSearchValue(event.target.value)}
+          placeholder="Search for movies..."
+        />
+      </div>
 
-            {/* Right-Aligned Search Box */}
-            <div className="col-sm-4">
-                <input
-                    type="text"
-                    className="form-control"
-                    value={searchValue}
-                    onChange={(event) => setSearchValue(event.target.value)}
-                    placeholder="Type to search..."
-                />
-            </div>
+      {/* Navigation Bar */}
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          {/* Title or Brand */}
+          <a className="navbar-brand" href="#">REEL RADAR</a>
+
+          {/* Toggler for Mobile View */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          {/* Nav Items */}
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              {/* Home Link */}
+              <li className="nav-item">
+                <a className="nav-link active" href="#home">Home</a>
+              </li>
+
+              {/* Favorites Link */}
+              <li className="nav-item">
+                <a className="nav-link" href="#favorites">Favorites</a>
+              </li>
+
+              {/* Genre Dropdown */}
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDropdown"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Genre
+                </a>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a className="dropdown-item" href="#action">Action</a></li>
+                  <li><a className="dropdown-item" href="#comedy">Comedy</a></li>
+                  <li><a className="dropdown-item" href="#drama">Drama</a></li>
+                  <li><a className="dropdown-item" href="#horror">Horror</a></li>
+                  <li><a className="dropdown-item" href="#romance">Romance</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-    );
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
+
