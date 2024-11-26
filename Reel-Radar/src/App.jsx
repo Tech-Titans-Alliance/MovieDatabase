0831b94691b80784b795dc4ada76ae4e359e9874
atlehang66 <atlehangsemela@gmail.com>
2024-11-26 14:52:55 +0200

    Home.jsx: Display Titans on page initial load

--- Reel-Radar/src/App.jsx
+++ Reel-Radar/src/App.jsx
@@ -1,55 +1,65 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import MovieList from './MovieList';
 import Header from './Header';
-
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
-import SearchBox from './SearchBox';
+import './App.css';
 
 
-const App=()=> {
-  const [movies, setMovies] = useState([]);
-  const [searchValue,setSearchValue]=useState('');
+const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchValue, setSearchValue] = useState('');
 
-  const GetMovieRequest=async(searchValue)=>{
-    const url=`http://www.omdbapi.com/?s=${searchValue}&apikey=c7301b93`
-    const response = await fetch(url);
-    const responseJson = await response.json();
-    console.log(responseJson);
-    if(responseJson.Search){
-      setMovies(responseJson.Search);
-    }
+    const GetMovieRequest = async (searchValue) => {
+        const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=c7301b93`;
+        // try {
+        //     const response = await fetch(url);
+        //     const responseJson = await response.json();
+        //     if (responseJson.Search) {
+        //         setMovies(responseJson.Search);
+        //     }
+        // } catch (error) {
+        //     console.error('Error fetching movies:', error);
+        // }
+
+
+        try {
+          const response = await fetch(url);
+          const responseJson = await response.json();
     
-  };
-  //for the api to fetch movies
-  useEffect(()=>{
-    GetMovieRequest(searchValue);
-  },[searchValue]);
+          if (responseJson.Search) {
+            setMovies(responseJson.Search);
+          } else {
+            // Default to fetching latest movies if no search results
+            const latestUrl = `http://www.omdbapi.com/?s=Teen Titans&apikey=c7301b93`;
+            const latestResponse = await fetch(latestUrl);
+            const latestResponseJson = await latestResponse.json();
+            if (latestResponseJson.Search) {
+              setMovies(latestResponseJson.Search);
+            } else {
+              console.error("No movies found, even for latest!");
+            }
+          }
+        } catch (error) {
+          console.error("Error fetching movies:", error);
+        }
+    };
 
-  return (
-    <>
-     
-    <div className='container-fluid'>
-       
-      {/* <div className='row d-flex align-items-center mt-4 mb-4'>
-        <Header/>
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
-        <Header/>
-      </div> */}
+    // Fetch movies when `searchValue` changes
+    useEffect(() => {
+        GetMovieRequest(searchValue);
+    }, [searchValue]);
 
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
-      
-     <div className='row'>
-      <MovieList movies={movies}/>
-    </div> 
-        
-    </div>
+    return (
+        <div className='container-fluid'>
+            {/* Header with SearchBox */}
+            <Header searchValue={searchValue} setSearchValue={setSearchValue} />
+            
+            {/* Movie List */}
+            <div className='row'>
+                <MovieList movies={movies} />
+            </div>
+        </div>
+    );
+};
 
-      
-    </>
-  )
-}
-
-export default App
+export default App;
